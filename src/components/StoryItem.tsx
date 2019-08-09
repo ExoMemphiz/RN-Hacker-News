4ebc3d323e0197228159bfe101e96a35b8ecb537
ExoMemphiz <ExoMemphiz@gmail.com>
2019-08-09 09:06:58 +0200

    Added modal

--- src/components/StoryItem.tsx
+++ src/components/StoryItem.tsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { IHackerNewsStory } from "../types/types";
+import { IStoryState } from "../reducers/StoryReducer";
 
-export default class StoryItem extends React.Component<IHackerNewsStory, {}> {
-	render() {
-		return (
-			<TouchableOpacity style={styles.container}>
-				<View style={styles.textContainer}>
-					<Text numberOfLines={1}>{this.props.title}</Text>
-				</View>
-			</TouchableOpacity>
-		);
-	}
+interface IStoryItemProps extends IHackerNewsStory, IStoryState {
+    onPress: (story: IHackerNewsStory) => void;
+}
+
+export default class StoryItem extends React.Component<IStoryItemProps, {}> {
+    render() {
+        return (
+            <TouchableOpacity onPress={() => this.props.onPress({ ...this.props })} style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text numberOfLines={1}>{this.props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "#CCC",
-		paddingTop: 10,
-		paddingBottom: 10,
-	},
-	textContainer: {
-		marginLeft: 20,
-		marginRight: 20,
-	},
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#EEE",
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    textContainer: {
+        marginLeft: 20,
+        marginRight: 20,
+    },
 });
--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -1,12 +1,11 @@
 import {
-	StyleSheet,
-	View,
-	Text,
-	TouchableOpacity,
-	ScrollView,
-	RefreshControl,
-	FlatList,
-	ActivityIndicator,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    RefreshControl,
+    FlatList,
 } from "react-native";
 import React from "react";
 import { connect } from "react-redux";
@@ -16,124 +15,152 @@ import { IStoryState } from "../reducers/StoryReducer";
 import { IStoryAction } from "../actions/StoryActions";
 import { getTopStories } from "../api/api";
 import StoryItem from "./StoryItem";
+import Modal from 'react-native-modal';
 import { IHackerNewsStory } from "../types/types";
 
 interface IDispatchProps {
-	loadStories: (loadType: "Single" | "All") => void;
-	changeLoadType: () => void;
-	changeSortOrder: () => void;
+    loadStories: (loadType: "Single" | "All") => void;
+    changeLoadType: () => void;
+    changeSortOrder: () => void;
 }
 
-interface IStoryListProps extends IStoryState, IDispatchProps {}
+interface IStoryListProps extends IStoryState, IDispatchProps { }
+
 interface IStoryListState {
-	refreshing: boolean;
+    refreshing: boolean;
+    modalInfo: undefined | IHackerNewsStory
 }
 
 class StoryList extends React.Component<IStoryListProps, IStoryListState> {
-	constructor(props: IStoryListProps) {
-		super(props);
+    constructor(props: IStoryListProps) {
+        super(props);
 
-		this.state = {
-			refreshing: false,
-		};
-	}
+        this.state = {
+            refreshing: false,
+            modalInfo: undefined
+        };
+    }
 
-	getRenderItem = () => {
-		let renderItem: undefined | Element = this.props.error && (
-			<Text>{this.props.error}</Text>
-		);
-		renderItem = renderItem || (this.props.loading && <Text>Loading...</Text>);
-		renderItem =
-			renderItem ||
-			(this.props.stories.length && (
-				<FlatList
-					data={this.props.stories}
-					renderItem={story => {
-						return <StoryItem {...story.item} />;
-					}}
-					keyExtractor={story => `${story.id}`}
-					refreshControl={
-						<RefreshControl
-							refreshing={this.state.refreshing}
-							onRefresh={this.handleRefresh}
-						/>
-					}
-				/>
-			));
-		return renderItem || <Text>Empty...</Text>;
-	};
+    handleRefresh = () => {
+        this.props.loadStories(this.props.loadType);
+    };
 
-	handleRefresh = () => {
-		this.props.loadStories(this.props.loadType);
-	};
+    renderModal = () => {
+        if (this.state.modalInfo) {
+            return (
+                <View style={styles.modalContent}>
+                    <Text style={{ color: "#FF6600" }}>{this.state.modalInfo.by}</Text>
+                </View>
+            )
+        }
+    }
 
-	render() {
-		return (
-			<ScrollView contentContainerStyle={styles.container}>
-				<View style={styles.loader}>
-					<FlatList
-						data={this.props.stories}
-						renderItem={story => {
-							return <StoryItem {...story.item} />;
-						}}
-						keyExtractor={story => `${story.id}`}
-						refreshControl={
-							<RefreshControl
-								refreshing={false}
-								onRefresh={this.handleRefresh}
-							/>
-						}
-					/>
-				</View>
-				<View style={{ flex: 1, marginTop: 20 }}>
-					<TouchableOpacity
-						onPress={() => this.props.loadStories(this.props.loadType)}
-					>
-						<Text>Load API</Text>
-					</TouchableOpacity>
-				</View>
-				<View style={{ flex: 1, marginTop: 10 }}>
-					<TouchableOpacity onPress={this.props.changeLoadType}>
-						<Text>{this.props.loadType}</Text>
-					</TouchableOpacity>
-				</View>
-				<View style={{ flex: 1, marginTop: 10 }}>
-					<TouchableOpacity onPress={this.props.changeSortOrder}>
-						<Text>{this.props.sortOrder}</Text>
-					</TouchableOpacity>
-				</View>
-			</ScrollView>
-		);
-	}
+    hideModal = () => {
+        this.setState({
+            modalInfo: undefined
+        })
+    }
+
+    showModal = (modalInfo: IHackerNewsStory) => {
+        console.log(`Showing Modal!`, modalInfo);
+        this.setState({
+            modalInfo
+        })
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Modal
+                    isVisible={this.state.modalInfo !== undefined}
+                    backdropColor="black"
+                    backdropOpacity={0.8}
+                    onBackdropPress={this.hideModal}
+                    animationOut="fadeOut"
+                    animationOutTiming={1000}
+                >
+                    <View style={{ flex: 1 }}>
+                        {this.renderModal()}
+                    </View>
+                </Modal>
+                <ScrollView contentContainerStyle={styles.container}>
+                    <View style={styles.loader}>
+                        <FlatList
+                            data={this.props.stories}
+                            renderItem={story => {
+                                // @ts-ignore
+                                return <StoryItem {...story.item} onPress={(story) => this.showModal(story)} />;
+                            }}
+                            keyExtractor={story => `${story.id}`}
+                            refreshControl={
+                                <RefreshControl
+                                    refreshing={false}
+                                    onRefresh={this.handleRefresh}
+                                />
+                            }
+                        />
+                    </View>
+
+                    <View style={{ flex: 1, marginTop: 20 }}>
+
+                        <TouchableOpacity
+                            onPress={() => this.props.loadStories(this.props.loadType)}
+                        >
+                            <Text>Load API</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 1, marginTop: 10 }}>
+                        <TouchableOpacity onPress={this.props.changeLoadType}>
+                            <Text>{this.props.loadType}</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 1, marginTop: 10 }}>
+
+                        <TouchableOpacity onPress={this.props.changeSortOrder}>
+                            <Text>{this.props.sortOrder}</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	loader: {
-		flex: 5,
-		marginBottom: 20,
-		marginTop: 20,
-	},
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    loader: {
+        flex: 5,
+        marginBottom: 20,
+        marginTop: 20,
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
 });
 
 function mapStateToProps(state: IState): IStoryState {
-	return state.stories;
+    return state.stories;
 }
 
 function mapDispatchToProps(dispatch: Dispatch<IStoryAction>): IDispatchProps {
-	return {
-		loadStories: (loadType: "Single" | "All") =>
-			getTopStories(loadType)(dispatch),
-		changeLoadType: () => dispatch({ type: "ChangeType" }),
-		changeSortOrder: () => dispatch({ type: "ChangeOrder" }),
-	};
+    return {
+        loadStories: (loadType: "Single" | "All") =>
+            getTopStories(loadType)(dispatch),
+        changeLoadType: () => dispatch({ type: "ChangeType" }),
+        changeSortOrder: () => dispatch({ type: "ChangeOrder" }),
+    };
 }
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps,
+    mapStateToProps,
+    mapDispatchToProps,
 )(StoryList);
